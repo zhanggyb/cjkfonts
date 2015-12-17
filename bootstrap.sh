3dd8b42df10709a3c1b85d927cada0be5836183f
Freeman Zhang <zhanggyb@gmail.com>
2015-12-17 11:34:47 +0800

    Remove Roboto,Source Sans Pro,Source Serif Pro.
    These fonts are already in texlive 2015 distribution.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-REPO_ROBOTO="https://github.com/google/roboto.git"
-REPO_SOURCE_SANS_PRO="https://github.com/adobe-fonts/source-sans-pro/archive/2.020R-ro/1.075R-it.tar.gz"
-REPO_SOURCE_SERIF_PRO="https://github.com/adobe-fonts/source-serif-pro/archive/1.017R.tar.gz"
 REPO_SOURCE_CODE_PRO="https://github.com/adobe-fonts/source-code-pro/archive/2.010R-ro/1.030R-it.tar.gz"
 REPO_SOURCE_HAN_SANS="https://github.com/adobe-fonts/source-han-sans/archive/1.004R.tar.gz"
 
@@ -23,18 +20,6 @@ trap "{ cd -; rm -rf $TMPDIR; exit 255; }" SIGINT
 
 cd $TMPDIR  # switch to temp dir
 echo "Change to ${TMPDIR}"
-
-# Get Google Roboto fonts
-git clone ${REPO_ROBOTO}
-cp -f roboto/out/*/*.ttf ${FONTDIR}
-
-# Get Adobe Source Sans Pro
-curl -L ${REPO_SOURCE_SANS_PRO} | tar zx
-cp -f source-sans-pro-*/OTF/*.otf ${FONTDIR}
-
-# Get Adobe Source Serif Pro
-curl -L ${REPO_SOURCE_SERIF_PRO} | tar zx
-cp -f source-serif-pro-*/OTF/*.otf ${FONTDIR}
 
 # Get Adobe Code Pro
 curl -L ${REPO_SOURCE_CODE_PRO} | tar zx
